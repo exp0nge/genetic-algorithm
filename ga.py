a429b5a4e380eb9c4a44989373a6d5963bd9f372
exp0nge <mdislamwork@gmail.com>
2017-05-08 21:15:07 -0400

    fix fitness func

--- ga.py
+++ ga.py
@@ -25,37 +25,41 @@ class GeneticAlgorithm(object):
         pass
 
     def fitness(self, permutation: Permutation):
-        """permutation is one permutation"""
+        """permutation is one Permutation with length = self.population_size"""
         for row in self.board:
             queen_col = permutation.arrangement[row]
-            print('row', row,'col', queen_col)
+            print('row', row, 'col', queen_col)
             # count queens in the same col
             permutation.score += permutation.arrangement.count(queen_col) - 1
             # count queens in the same row
             permutation.score += permutation.arrangement.count(row) - 1
 
+            print("perm before diagnols", permutation.score)
             # check the four diagnols
-            for i in range(row, self.population_size):
-                for j in range(queen_col, self.population_size):
-                    # + i, + j -> up, right
-                    up = row - j
-                    down = row + i
-                    right = queen_col + j
-                    left = queen_col - j
-                    if up >= 0 and up != row:
-                        queen = permutation.arrangement[up]
-                        if queen == left or queen == right:
-                            permutation.score += 1
-                    if down < self.population_size and down != row:
-                        queen = permutation.arrangement[down]
-                        if queen == left or queen == right:
-                            permutation.score += 1
-                print(permutation.score)
+            for i in range(0, self.population_size):
+                up = row - i
+                down = row + i
+                right = queen_col + i
+                left = queen_col - i
+
+                if (up == row or down == row) and (right == queen_col or left == queen_col):
+                    # don't count the queen we are currently checking
+                    continue
+
+                if up >= 0:
+                    queen = permutation.arrangement[up]
+                    if queen == left or queen == right:
+                        permutation.score += 1
+                if down < self.population_size:
+                    queen = permutation.arrangement[down]
+                    if queen == left or queen == right:
+                        permutation.score += 1
+            print("perm score", permutation.score)
 
 if __name__ == '__main__':
     n = 4
     ga = GeneticAlgorithm(n)
-    perm = Permutation(n, list(range(n)))
+    perm = Permutation(n, [3, 1, 0, 2])
     print(perm.arrangement)
     ga.fitness(perm)
     print("total", perm.score)
