abdea9eff09498df4ec9a2cf26ede33f570cd6d7
exp0nge <mdislamwork@gmail.com>
2017-05-08 23:09:51 -0400

    Implement selection, crossover, mutation

--- ga.py
+++ ga.py
@@ -1,77 +1,116 @@
+"""Genetic algorithm using roulette selection, crossover, and mutation"""
 import itertools
+import random
 
 
 class Permutation(object):
+    """Represents a list of integers"""
     def __init__(self, N: int, arrangement: list, fitness: int=0):
         self.population_size = N
         self.fitness = fitness or 0
         self.arrangement = arrangement
 
 
+class GoalFound(Exception):
+    pass
+
+
 class GeneticAlgorithm(object):
     """index: row, value: row"""
     def __init__(self, N: int):
         self.population_size = N
+        self.goal_fitness = (self.population_size - 1) * self.population_size
         self.board = list(range(self.population_size))
-        total_fitness = 0
-
         self.permutations = []
-        for permutation in itertools.permutations(range(self.population_size)):
-            fitness = self.fitness(permutation)
-            total_fitness += fitness
-            self.permutations.append(Permutation(self.population_size, permutation, fitness))
+        for arrangement in itertools.permutations(range(self.population_size)):
+            print("Creating permutation", arrangement)
+            permutation = Permutation(self.population_size, arrangement)
+            self.fitness(permutation)
+            self.permutations.append(permutation)
 
-        self.avg_fitness = float(total_fitness) / self.population_size
+    def total_fitness(self):
+        """Returns the total fitness of all permutations"""
+        return sum([permutation.fitness for permutation in self.permutations])
 
     def apply_roulette_selection(self):
-        """Apply fi/favg to roulette"""
+        """Apply fi/favg to get roulette"""
         roulette = []
-        for permutation in self.permutations:
-            roulette += [permutation] * (permutation.fitness / self.avg_fitness)
-        return roulette
+        for perm in self.permutations:
+            roulette += [perm] * (perm.fitness / (self.total_fitness() / self.population_size))
+        self.permutations = roulette
 
     def apply_crossover(self):
-        pass
+        """Take 2 random mates, select random crossover point. Produce two offsprings"""
+        crossed_over = []
+        for _ in range(self.population_size / 2):
+            crossover_point = random.randint(0, self.population_size - 1)
+            parent_one = self.permutations[random.randint(0, self.population_size - 1)]
+            parent_two = self.permutations[random.randint(0, self.population_size - 1)]
 
-    def apply_mutation(self):
-        pass
+            crossed_over.append(Permutation(self.population_size,
+                                            parent_one.arrangement[:crossover_point] +
+                                            parent_two[crossover_point:]))
+            crossed_over.append(Permutation(self.population_size,
+                                            parent_two.arrangement[:crossover_point] +
+                                            parent_one[crossover_point:]))
+        self.permutations = crossed_over
+
+    def apply_mutation(self, rate: float):
+        """Change the queens of permutation based on probabilistic rate"""
+        for permutation in self.permutations:
+            for index in self.board:
+                if random.random() > rate:
+                    permutation.arrangement[index] = random.randint(0, self.population_size - 1)
 
     def fitness(self, permutation: Permutation):
         """permutation is one Permutation with length = self.population_size"""
         for row in self.board:
             queen_col = permutation.arrangement[row]
-            print('row', row, 'col', queen_col)
             # count queens in the same col
             permutation.fitness += permutation.arrangement.count(queen_col) - 1
             # count queens in the same row
             permutation.fitness += permutation.arrangement.count(row) - 1
 
-            print("perm before diagnols", permutation.fitness)
             # check the four diagnols
             for i in range(0, self.population_size):
-                up = row - i
+                _up = row - i
                 down = row + i
                 right = queen_col + i
                 left = queen_col - i
 
-                if (up == row or down == row) and (right == queen_col or left == queen_col):
+                if (_up == row or down == row) and (right == queen_col or left == queen_col):
                     # don't count the queen we are currently checking
                     continue
 
-                if up >= 0:
-                    queen = permutation.arrangement[up]
+                if _up >= 0:
+                    queen = permutation.arrangement[_up]
                     if queen == left or queen == right:
                         permutation.fitness += 1
                 if down < self.population_size:
                     queen = permutation.arrangement[down]
                     if queen == left or queen == right:
                         permutation.fitness += 1
-            print("perm fitness", permutation.fitness)
+
+            permutation.fitness = self.goal_fitness - permutation.fitness
+
+            if permutation.fitness == self.goal_fitness:
+                raise GoalFound("Goal with fitness", permutation.fitness, permutation.arrangement)
+
+            return permutation.fitness
+
+    def generate(self, count: int, mutation_rate: float):
+        """Apply roulette, crossover, and mutations count times with rate"""
+        for _ in range(count):
+            print("Apply roulette", len(self.permutations))
+            self.apply_roulette_selection()
+            print("Apply crossover", len(self.permutations))
+            self.apply_crossover()
+            print("Apply mutation", len(self.permutations))
+            self.apply_mutation(mutation_rate)
+
 
 if __name__ == '__main__':
-    n = 4
-    ga = GeneticAlgorithm(n)
-    perm = Permutation(n, [3, 1, 0, 2])
-    print(perm.arrangement)
-    ga.fitness(perm)
-    print("total", perm.fitness)
+    ga = GeneticAlgorithm(20)
+    print("before generate fitness: ", ga.total_fitness())
+    ga.generate(1, 0.001)
+    print("before generate fitness: ", ga.total_fitness())
